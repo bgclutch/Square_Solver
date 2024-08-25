83c48a7748b56180aa4f6df94192902670659e8e
Bogdan <bvolsky2006@gmail.com>
2024-08-25 13:01:07 +0300

    added colors

--- sqr_solv_structs.h
+++ sqr_solv_structs.h
@@ -1,6 +1,7 @@
 
 #ifndef SQR_SOLV_STRUCTS_H_
 #define SQR_SOLV_STRUCTS_H_
+#include <string.h>
 
 enum NumRoots
 {
@@ -13,8 +14,8 @@ enum NumRoots
 struct Equation_Coefficients_Data
 {
     double sqr_x_coef; ///< coefficient belongs x^2
-    double x_coef;    ///< coefficient belongs x
-    double free_coef; ///< free coefficient 
+    double x_coef;     ///< coefficient belongs x
+    double free_coef;  ///< free coefficient 
 };
 
 struct Equation_Attributes_Data
@@ -23,6 +24,14 @@ struct Equation_Attributes_Data
     double root_1;        ///< first (or one for lineal equation) root
     double root_2;        ///< second root
     NumRoots root_number; ///< number of roots
+};
+
+struct Colorized_Output colors = 
+{
+    string red =   "\e[0;31m", ///< red
+    string green = "\e[0;32m", ///< green
+    string blue =  "\e[0;34m", ///< blue
+    string del_col =  "\e[0m"  ///< reset color
 };
 
 #endif // SQR_SOLV_STRUCTS_H_
